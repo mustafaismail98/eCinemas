bbebe23d89b3acff24cd83cf562ddfe02b0cab07
mustafais481 <mustafais481@gmail.com>
2023-06-19 22:23:04 +0200

    feat(database): add first migration with ef core

--- eCinemas/Startup.cs
+++ eCinemas/Startup.cs
@@ -2,6 +2,7 @@ using eCinemas.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +25,7 @@ namespace eCinemas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddControllersWithViews();
         }
 
