31739e378a69eac2e93894ee6029187e571a1ddd
mustafais481 <mustafais481@gmail.com>
2023-07-03 22:05:13 +0200

    feat(ordersController): add Add item and Remove item from cart feature

--- eCinemas/Controllers/OrdersController.cs
+++ eCinemas/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using eCinemas.Data.Services;
 using eCinemas.Data.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace eCinemas.Controllers
 {
@@ -28,6 +29,30 @@ namespace eCinemas.Controllers
             };
 
             return View(response);
+        }
+
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _moviesService.GetByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+
+            return RedirectToAction(nameof(ShoppingCart));
+
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
         }
     }
 }
