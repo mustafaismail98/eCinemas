87cb593dfdc1cb8c1b984887b4f253b0619aa931
mustafais481 <mustafais481@gmail.com>
2023-07-04 23:21:09 +0200

    feat(ShoppingCartSummary): ajout du panier

---
+++ eCinemas/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,26 @@
+﻿using eCinemas.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCinemas.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        //Get the number of items in the shopping cart 
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+
+
+
+        }
+    }
+}
